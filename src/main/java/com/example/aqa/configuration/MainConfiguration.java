eb05b8a10703edb8379330603627aa570ae00d82
alekseyn <alekseyn@playtika.com>
2025-08-12 11:16:11 +0200

    Added configuration profiles

--- src/main/java/com/example/aqa/configuration/MainConfiguration.java
+++ src/main/java/com/example/aqa/configuration/MainConfiguration.java
@@ -4,11 +4,10 @@ import com.example.aqa.configuration.extension.ExtensionConfiguration;
 import com.example.aqa.configuration.rest.RestApiClientConfiguration;
 import com.example.aqa.configuration.driver.appium.AppiumConfiguration;
 import com.example.aqa.driver.AppDriver;
+import com.example.aqa.driver.AppiumBasedAppDriver;
 import com.example.aqa.driver.MockAppDriver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import io.appium.java_client.AppiumDriver;
+import org.springframework.context.annotation.*;
 
 /**
  * Entry point Spring configuration assembling all framework pieces.
@@ -28,8 +27,21 @@ public class MainConfiguration {
      * @return mock implementation of the application driver
      */
     @Bean
-    public AppDriver appDriver() {
+    @Profile("mock")
+    public AppDriver mockAppDriver() {
         return new MockAppDriver();
+    }
+
+    /**
+     * Provides the {@link AppDriver} used in tests when running with Appium.
+     *
+     * @param appiumDriver the Appium driver instance
+     * @return Appium-based implementation of the application driver
+     */
+    @Bean
+    @Profile("appium")
+    public AppDriver appiumBasedAppDriver(AppiumDriver appiumDriver) {
+        return new AppiumBasedAppDriver(appiumDriver);
     }
 
 }
--- src/main/java/com/example/aqa/configuration/driver/appium/AppiumConfiguration.java
+++ src/main/java/com/example/aqa/configuration/driver/appium/AppiumConfiguration.java
@@ -6,29 +6,32 @@ import org.openqa.selenium.Capabilities;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Duration;
 
 /**
  * Placeholder configuration for an Appium-based driver.
  * Uncomment the example beans to enable real Appium support.
  */
+@Profile("appium")
 @Configuration
 @EnableConfigurationProperties(AppiumProperties.class)
 public class AppiumConfiguration {
 
-//    @Bean
-//    public Capabilities capabilities(AppiumProperties properties) {
-//        return new UiAutomator2Options()
-//                .setNewCommandTimeout(Duration.ofMinutes(properties.getTimeOut()))
-//                .setDeviceName(properties.getDevice())
-//                .setApp(properties.getApp());
-//    }
+    @Bean
+    public Capabilities capabilities(AppiumProperties properties) {
+        return new UiAutomator2Options()
+                .setNewCommandTimeout(Duration.ofMinutes(properties.getTimeOut()))
+                .setDeviceName(properties.getDevice())
+                .setApp(properties.getApp());
+    }
 
-//    @Bean(destroyMethod = "quit")
-//    public AppiumDriver appiumDriver(Capabilities capabilities, AppiumProperties properties) throws MalformedURLException {
-//        return new AppiumDriver(new URL( String.format("%s:%d", properties.getHost(), properties.getPort())), capabilities);
-//    }
+    @Bean(destroyMethod = "quit")
+    public AppiumDriver appiumDriver(Capabilities capabilities, AppiumProperties properties) throws MalformedURLException, URISyntaxException {
+        return new AppiumDriver(new URI(String.format("%s:%d", properties.getHost(), properties.getPort())).toURL(), capabilities);
+    }
 }
